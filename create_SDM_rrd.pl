5d089ee1b9c11057cdf615e872fb0737b25ceb11
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-06 19:24:42 +0100

    crafting option processing

--- create_SDM_rrd.pl
+++ create_SDM_rrd.pl
@@ -30,7 +30,35 @@ use DateTime;
 use Data::Dumper  ;
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+our $Debug = 3;
+require ('./my_debugs.pl');
+
+
+
 my $retval = getopts('dtfad:p:c:r:lv:h');
 die "$usage" unless ($retval) ;
-die "$usage" if $opt_h  ;
+die "$usage" if my $opt_h  ;
+
+our $sdm_def_file;
+our (@SDM_regs , %SDM_reg_by_tag , %SDM_selectors);
+require ('./extract-SDM-def.pm');
+
+our %Counterlist;
+our @all_selectors;
+require ('./my_counters.pm');
+
+
+our ( $RRD_dir , $RRD_prefix , $RRD_sprintf );
+our %RRD_definitions ;
+require ('./rrd_def.pm');
+
+print Data::Dumper->Dump (
+	[ \@SDM_regs , \%SDM_reg_by_tag , \%SDM_selectors , \@all_selectors , \%Counterlist  ] ,
+	[ qw(*SDM_regs  *SDM_reg_by_tag   *SDM_selectors  *all_selectors       *Counterlist ) ]  );
+
+print Data::Dumper->Dump ( [ \%RRD_definitions ]  , [ qw(%RRD_definitions) ]  );
+
+
 
