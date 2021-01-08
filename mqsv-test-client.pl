b3a9d74ad2822faea8f44e7ec29bc51ac0cd07c5
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-09 00:20:36 +0100

    remove other tests garbage

--- mqsv-test-client.pl
+++ mqsv-test-client.pl
@@ -6,7 +6,7 @@ use warnings;
 use IPC::SysV qw(IPC_PRIVATE S_IRUSR S_IWUSR ftok IPC_CREAT IPC_NOWAIT );
 use IPC::Msg();
 use Cwd qw( realpath );
-use Time::HiRes () ;
+# use Time::HiRes () ;
 
 my $mq_ref = "./message_queue.kilroy" ;
 my $mq_mtype = 1;
@@ -21,14 +21,6 @@ my $MQ  = IPC::Msg->new($our_ftok     ,   S_IWUSR | S_IRUSR |  IPC_CREAT )
 
 my $cnt =1;
 while (1) {
-  # rolling ms
-  # my $ts = (int (Time::HiRes::time * 1000)) % 0x100000000 ;
-  # my $msg = sprintf ("%08x:%08x::GS" , $ftok_my, $ts );
-  # print "sending .... ", $msg ;
-  # $mq_srv->snd (1, $msg );
-  # print " ... done \n";
-
-  # sleep 1;
 
   my $buf;
   # $mq_my->rcv($buf, 1024, 1 , IPC_NOWAIT  );
