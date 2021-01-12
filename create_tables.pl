9a668ea2d311a6845ef1d308d184736934da0c02
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-12 08:03:44 +0100

    ...creating table heads

--- create_tables.pl
+++ create_tables.pl
@@ -3,26 +3,68 @@
 # print debug to SDTERR
 # working output to file
 
-
+use warnings;
+use strict;
 use Data::Dumper;
 
+# "%s/%s_%s_%s.rrd"; 
+our ( $RRD_dir , $RRD_prefix , $RRD_sprintf );
+require ('./rrd_def.pm');
 
-# 
+
+
+#  import database strucutre - we may run this on console to debug
+#  tag tree of  %sql_tables->{counter}->{rrd}->{field}
+#  
 my %sql_tables;
 my $sql_tables_dump = `./test-SQL-def.pl 2> /dev/null`;
 
-# my %sql_tables = eval ($sql_tables_dump );
-
 eval ($sql_tables_dump );
-
-# my $sql_tables = eval "(foo => bar)" ;
-
 print $@ ; # eval error message
-print "hello? \n";
-# print $sql_tables_dump;
-print "hello again \n";
 print Dumper ( \%sql_tables ) ;
 
-eval ' @foo = qw ( pi pa po ) ';
-print @foo ;
-print Dumper ( \@foo ) ;
+# database chunks - basically copy over from infini
+
+my $outer_head = <<"EOF_OHEAD";
+/*!40101 SET character_set_client = utf8 */;
+EOF_OHEAD
+
+my $tabdef_head = <<"EOF_TDHEAD";
+DROP TABLE IF EXISTS `%s`;
+CREATE TABLE `%s` (
+  `time` datetime NOT NULL,
+EOF_TDHEAD
+
+# hope that a generic float type meets best 
+my $tabdef_row = <<"EOF_TDROW" ;
+  `%s` float DEFAULT NULL,
+EOF_TDROW
+  
+my $tabdef_tail = <<"EOF_TDTAIL";
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  PRIMARY KEY (`time`)
+) ENGINE=MyISAM DEFAULT CHARSET=ascii ;
+EOF_TDTAIL
+
+
+print STDERR "========== start parsing data tree \n ==========";
+
+# prelude
+print $outer_head;
+
+# cycle over tables
+
+my $db_sprintf = $RRD_sprintf ;
+# $db_sprintf =~ s/\.rrd$/.sql/ ;
+
+
+
+
+
+for my $counter_tag ( sort keys %sql_tables ) {
+	my $table_list_p = $sql_tables{ $counter_tag } or next ;
+	for my $table_tag ( sort keys %$table_list_p ) {
+		my $tablename = sprintf "%s_%s_%s", $RRD_prefix , $counter_tag, $table_tag ;
+		printf $tabdef_head, $tablename, $tablename ;
+	}
+}
