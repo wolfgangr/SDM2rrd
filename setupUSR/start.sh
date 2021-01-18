f074db1d629b046e9076715b07fc5dbe808b8158
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-18 20:32:20 +0100

    boilerplating systemd watchdog & stuff

---
+++ setupUSR/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# systemd starter script
+
+SCRIPTDIR=`dirname "$0"`
+# echo $SCRIPTDIR
+cd $SCRIPTDIR
+
+# spawn our associated babysitter
+./watchdog.pl &
+
+# not sure what environment we get from systemd
+# echo $PATH
+# pwd
+cd ..
+# source /etc/profile
+# source ~/.profile
+# echo $PATH
+# pwd
+# launch the real thing
+./USR-SDM-Poller.pl > /dev/null
+
+# don' return to systemd, this breaks `notify` setting 
