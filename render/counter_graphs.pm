7de0a45237bb949494901c3c1d233f1b70ab2e9e
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-19 16:26:08 +0100

    modularize dynamic generation

---
+++ render/counter_graphs.pm
@@ -0,0 +1,23 @@
+# implement the dynamic generation of graph specs for counters
+
+
+# all the heredocs 
+our $rrd_tpl_mains_stacked;
+our $rrd_tpl_mains_lined;
+
+
+require './counter_graph_templates.pm';
+
+
+
+
+
+
+# replace the static rrd-graph files
+# @tail_lines = graph_spec ($counter, $template) 
+
+
+
+
+
+;
