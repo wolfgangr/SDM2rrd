2a8ea2b009de9387e55d14782480e9eab556ccd1
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-19 17:41:30 +0100

    start boilerplating subs

--- render/counter_graphs.pm
+++ render/counter_graphs.pm
@@ -1,12 +1,15 @@
 # implement the dynamic generation of graph specs for counters
 
+use Cwd();
 
 # all the heredocs 
 our $rrd_tpl_mains_stacked;
 our $rrd_tpl_mains_lined;
-
-
 require './counter_graph_templates.pm';
+
+
+$rrdpath = Cwd::realpath ( '../rrd');
+
 
 # load stored counterlist
 our $counterlist_f = './counterlist.dat';
@@ -24,24 +27,62 @@ our %target_h = (
         mains_d =>  \@target_any ,
 ) ;
 
-# all subs? counter get the default
+# all subs# counter get the default
 for my $cnt ( grep { /subs\d/ }  keys %counterlist ) {
         $target_h{ $cnt } = \@target_any ;
 }
 
 
+my %counter_default_colors = ( 
+	mains => '0000FF' ,  
+	mains_d => '0000aa',
+	subs1 => 'FFA500', 
+	subs2 => '008000', 
+	subs3 => '800080', 
+	subs4 => 'FFD700', 
+	subs5 => 'FF0000', 
+	subs6 => '00FF00', 
+) ;
 
 
 
+#~~~~~ end of setup stuff~~~~~~~~~~~~~~~~~~~
+
+#=================== called by CGI ===============================
 # replace the static rrd-graph files
 # @tail_lines = graph_spec ($counter, $template) 
 sub graph_spec {
 	my ($counter, $template) = @_ ;
 	my @rvs;
 
-	@rvs = rrdg_lines_ary ($rrd_tpl_mains_stacked);
-	return @rvs;
+	if ($counter eq 'mains' and  $template eq 'm_stacked' ) {
+		return dummy2_spec() ;
+	}
+	# @rvs = rrdg_lines_ary ($rrd_tpl_mains_stacked);
+	# return @rvs;
+	#
+	# ... if (noting ( else )) { ....
+	return dummy_spec() ;
 }
+
+
+# 
+sub main_area_spec {
+	my $counterlist = shift;
+
+}
+
+#~~~~~~~~~~~~~~~~~~~~ helper subs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+sub dummy_spec {
+	return rrdg_lines_ary ($rrd_tpl_mains_stacked);
+}
+
+
+sub dummy2_spec {
+        return rrdg_lines_ary ($rrd_tpl_mains_lined);
+}
+
 
 
 # filter rrd graph lines
